6846756974b213dca525d6fc06460312b575f68d
Thiago Rose <thiago.senhorinha@gmail.com>
2014-06-08 08:42:31 -0300

    Alterando atributo identificador para string.

--- src/ControleProcesso.cpp
+++ src/ControleProcesso.cpp
@@ -1,8 +1,8 @@
 #include "include/ControleProcesso.h"
 
 //Informações do Processo 
-ControleProcesso::ControleProcesso(int _pid, int _tChegada, int _tExec, double _size){
-	pid = _pid;
+ControleProcesso::ControleProcesso(string nome, int _tChegada, int _tExec, double _size){
+	this->nome = nome;
 	tChegada = _tChegada;
 	tExec = _tExec;
 	size = _size;	
@@ -10,14 +10,14 @@ ControleProcesso::ControleProcesso(int _pid, int _tChegada, int _tExec, double _
 }
 //Usado para o segmento não alocado da memoria
 ControleProcesso::ControleProcesso(){
-	pid = -1;       
+	nome = "free";
 	tChegada = 0;
 	tExec = 0;
 	size = 0;	
 
 }
-ControleProcesso::ControleProcesso(int _pid, int _tChegada, int _tExec, int _size){
-	pid = _pid;
+ControleProcesso::ControleProcesso(string nome, int _tChegada, int _tExec, int _size){
+	this->nome = nome;
 	tChegada = _tChegada;
 	tExec = _tExec;
 	size = _size;
--- src/include/ControleProcesso.h
+++ src/include/ControleProcesso.h
@@ -1,18 +1,21 @@
 #ifndef CONTROLEPROCESSO_H
 #define CONTROLEPROCESSO_H
 
-class ControleProcesso
-{
-   
-public:
-int pid;
-   int tChegada;
-   int tExec;
-   int size;
+#include <iostream>
 
-   ControleProcesso(int _pid, int _tChegada, int _tExec, double _size);
-   ControleProcesso(int _pid, int _tChegada, int _tExec, int _size);
-   ControleProcesso();
+using namespace std;
+
+class ControleProcesso {
+
+public:
+	string nome;
+	int tChegada;
+	int tExec;
+	int size;
+
+	ControleProcesso(string nome, int _tChegada, int _tExec, double _size);
+	ControleProcesso(string nome, int _tChegada, int _tExec, int _size);
+	ControleProcesso();
 };
 
 #endif
