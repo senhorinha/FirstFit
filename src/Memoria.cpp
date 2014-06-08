ea36896267e8044a9580e3aea2dbce27d5ed5087
Thiago Rose <thiago.senhorinha@gmail.com>
2014-06-08 08:43:58 -0300

    Refatorando para utilizar nome como identificador do processo

--- src/Memoria.cpp
+++ src/Memoria.cpp
@@ -29,7 +29,7 @@ int Memoria::insertBloco(Bloco _bloco)
 	int posicaoAlocado = 0;
 	//alocar um processo caso possua tamanho suficiente e não tiver alocado por outro processo;
 	for(list<Bloco>::iterator it=mem.begin();it!=mem.end();++it){
-		if(it->proc.pid == FREE){
+		if(it->proc.nome == FREE){
 			if(it->size >= _bloco.size){
 				//espaço suficiente, então alocar
 				it->tamanhoRestante = it->size - _bloco.size;
@@ -45,15 +45,15 @@ int Memoria::insertBloco(Bloco _bloco)
 }
 void Memoria::exibir(){
 	for(list<Bloco>::iterator it = mem.begin();it != mem.end();++it){
-		printf("bloco PID %i | tamanho do bloco %i | espaço restante %i \n",it->proc.pid,it->size,it->tamanhoRestante);
+		printf("bloco PID %i | tamanho do bloco %i | espaço restante %i \n",it->proc.nome,it->size,it->tamanhoRestante);
 	}
 }
 int Memoria::removeBloco(Bloco _bloco){
 	//procurar um bloco alocado por seu PID;
 	int pos = 0;
 	for(list<Bloco>::iterator it = mem.begin();it !=mem.end();++it){
-		if(it->proc.pid == _bloco.proc.pid){
-			it->proc.pid = -1;
+		if(it->proc.nome == _bloco.proc.nome){
+			it->proc.nome = "free";
 			it->tamanhoRestante = it->size;
 			return pos;
 		}
